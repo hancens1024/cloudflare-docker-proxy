fdbf83426bd2f69563167603ce41be6d3ef9213b
hancens <41316193+hancens1024@users.noreply.github.com>
2024-07-22 15:56:16 +0800

    chore: change my domain

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.hancens.com": dockerHub,
+  "quay.hancens.com": "https://quay.io",
+  "gcr.hancens.com": "https://gcr.io",
+  "k8s-gcr.hancens.com": "https://k8s.gcr.io",
+  "k8s.hancens.com": "https://registry.k8s.io",
+  "ghcr.hancens.com": "https://ghcr.io",
+  "cloudsmith.hancens.com": "https://docker.cloudsmith.io",
+  "ecr.hancens.com": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.hancens.com": dockerHub,
 };
 
 function routeByHosts(host) {
